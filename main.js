e7785e4749f25c3f934193a461b3adf3f3cabfe2
Josh Kelly <jkelly85@alumni.uwo.ca>
2024-02-22 19:13:47 -0500

    forward/reverse controls wusing speed and acceleration

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 const canvas = document.getElementById("myCanvas");
-canvas.height = innerHeight;
 canvas.width = 200;
 
 const ctx = canvas.getContext("2d");
 const car = new Car(100, 100, 30, 50);
 car.draw(ctx);
 
+animate();
 
-const animate = function() {
+function animate() {
   car.update();
+  canvas.height = window.innerHeight;
   car.draw(ctx);
   requestAnimationFrame(animate);
 }
-animate();
