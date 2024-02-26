a056d10f4f07abe4942b4e97a2d2e6f835fd5242
Josh Kelly <jkelly85@alumni.uwo.ca>
2024-02-26 16:00:24 -0500

    move lerp to utils, dash road lane lines

--- road.js
+++ road.js
@@ -22,14 +22,15 @@ class Road {
         this.right,
         i / this.laneCount
       );
+      if (i > 0 && i < this.laneCount) {
+        ctx.setLineDash([20, 20]);
+      } else {
+        ctx.setLineDash([]);
+      }
       ctx.beginPath();
       ctx.moveTo(x, this.top);
       ctx.lineTo(x, this.bottom);
       ctx.stroke();
     }
   }
-};
-
-function lerp(A, B, t) {
-  return A + (B - A) * t;
 };
