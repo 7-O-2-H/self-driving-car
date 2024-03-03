e63112a9fd6a8956839b824c8417b450c1107858
Josh Kelly <jkelly85@alumni.uwo.ca>
2024-03-02 21:05:23 -0500

    broken collision detector with obstacles

--- sensor.js
+++ sensor.js
@@ -9,15 +9,23 @@ class Sensor {
     this.readings = [];
   }
 
-  update(roadBorders) {
+  update(roadBorders, traffic) {
     this.#castRays();
     this.readings = [];
     for (let i = 0; i < this.rays.length; i++) {
-      this.readings.push(this.#getReading(this.rays[i], roadBorders));
+      this.readings.push(
+        this.#getReading(
+          this.rays[i], 
+          roadBorders, 
+          traffic
+        )
+      );
+      
     }
   }
 
-  #getReading(ray, roadBorders) {
+  
+  #getReading(ray, roadBorders, traffic) {
     let touches = [];
 
     for(let i = 0; i < roadBorders.length; i++) {
@@ -32,6 +40,21 @@ class Sensor {
       }
     }
 
+    for (let i = 0; i < traffic.length; i++) {
+      const poly =  traffic[i].polygon;
+      for (let j = 0; j < poly.length; j++) {
+        const value = getIntersection(
+          ray[0],
+          ray[1],
+          poly[j],
+          poly[(j + 1) % poly/length]
+        );
+        if (value) {
+          touches.push(value);
+        }
+      }
+    }
+   
     if (touches.length === 0) {
       return null;
     } else {
@@ -39,7 +62,7 @@ class Sensor {
       const minOffset = Math.min(...offsets);
       return touches.find((e) => e.offset === minOffset);
     }
-  } 
+  }
   
 
   #castRays() {
