4444390fded3d7a8ad9d0234cab8075ae11b461a
Josh Kelly <jkelly85@alumni.uwo.ca>
2024-03-08 16:57:34 -0500

    add mutate method

--- main.js
+++ main.js
@@ -13,7 +13,12 @@ const cars = generateCars(N);
 
 let bestCar = cars[0];
 if (localStorage.getItem("bestBrain")) {
-  bestCar = JSON.parse(localStorage.getItem("bestBrain"));
+  for (let i = 0; i < cars.length; i++) {
+    cars[i].brain = JSON.parse(localStorage.getItem("bestBrain"));
+    if (i !== 0) {
+      NeuralNetwork.mutate(cars[i].brain, 0.2);
+    }
+  }
 };
 
 const traffic = [
